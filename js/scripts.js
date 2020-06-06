d1fd863f98b3bcbdbdf80395fff13731f3d56ca3
tomsarry <55455075+tomsarry@users.noreply.github.com>
2020-06-06 19:33:03 -0400

    get email extension, add and removes table rows

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,31 @@
-let btnSubmit = document.getElementById('btnSubmit');
-let formInput = document.getElementById('formInput');
+var btnSubmit = document.getElementById('btnSubmit');
+var formInput = document.getElementById('formInput');
+var table = document.getElementById('tableResult');
+
 
 //find the "link"
-var reLink = /(\w+\.\w+)+/g;
+var reLink = /(\w+\.[^\/]+)+/g;
 
 // regex to find the extension
-var reDot = /(\.\w+)+/g;
+var reDot = /(\.\w+)+$/g;
 
-var links, extensions, userInput;
+var links, extension, extensions, userInput;
 
 btnSubmit.addEventListener('click', validate);
-// btnSubmit.onclick = getExtension;
 
 function validate() {
+    // remove every pre existing table row
+    if (table.rows.length > 1) { clearTable(); }
+
     userInput = formInput.value;
     if (! userInput.includes(".")) { 
         // error message
+        displayError();
         return; 
     }
 
-    // valide email adresses !!
-
     // if string validated ; get the extension
-    getExtension(userInput);
-    
+    getExtension(userInput);  
 }
 
 function getExtension(userInput) {
@@ -31,17 +33,31 @@ function getExtension(userInput) {
     // get each file/link
     links = userInput.match(reLink);
 
-    // display to the user what he typed
-    links.forEach(function(name) {
-        original.innerHTML += name + '\n';
-    })
-
-    
- 
-
-    extensions = userInput.match(reDot);
-
-    extensions.forEach(function(item) {
-        result.innerHTML += item + '\n'; 
+    links.forEach(function(element) {
+        extension = element.match(reDot);
+        addRow(element, extension);
     });
+}
+
+function addRow(original, extension) {
+    let numRows = table.rows.length;
+    let newLine = table.insertRow(numRows);
+    let firstCell = newLine.insertCell(0);
+    let secondCell = newLine.insertCell(1);
+
+    firstCell.innerHTML = original;
+    secondCell.innerHTML = extension;
+}
+
+//TODO
+function displayError() {
+
+}
+
+function clearTable() {
+    let numRows = table.rows.length;
+    console.log(numRows);
+    for (let i = numRows-1; i > 0; i--) {
+        table.deleteRow(i);
+    }
 }
